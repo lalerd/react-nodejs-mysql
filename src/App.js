9a27db6cdf57545a88e0e146f5b818442e6964c6
Lale Erdogan <lalerdgn@gmail.com>
2022-01-09 02:53:55 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Axios from 'axios'
+
+function App() {
+
+  const [city, setCity] = useState('');
+  const [cityList, setCityList] = useState([]);
+
+  useEffect(()=> {
+    Axios.get('http://localhost:3001/api/get').then((response)=> {
+      setCityList(response.data);
+    })
+  }, [])
+
+  const addCity = () =>{
+     Axios.post('http://localhost:3001/api/insert', {
+       city: city
+      })
+        setCityList([
+          ...cityList, 
+          {city: city}
+        ])
+     
+  }
+
+
+  return (
+    <div className="App">
+     <h2>Cities</h2>
+     <div className="form">
+     <input type="text" name="city"
+     onChange={(e)=>{
+       setCity(e.target.value);
+     }} />
+
+     <button onClick={addCity}>ADD</button>
+     <hr/>
+     {cityList.map((val)=>{
+        return (
+        <div className='card'> 
+        <p>{val.city}</p>
+        </div>
+        )
+     })}
+     </div>
+    </div>
+  );
+}
+
+export default App;
